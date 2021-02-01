411e4f29ce6e353cdf15a70edc7f5fdab870987b
Inga Oberemchuk <inga_oberemchuk@mail.ru>
2021-02-01 23:44:25 +0300

    Test with a mistake

--- src/test/java/AppOrderFormTest.java
+++ src/test/java/AppOrderFormTest.java
@@ -10,7 +10,7 @@ public class AppOrderFormTest {
     void shouldAppOrderTest() {
         open("http://localhost:9999");
         $("[data-test-id=name] input").setValue("Иван Иванович");
-        $("[data-test-id=phone] input").setValue("+71111111111");
+        $("[data-test-id=phone] input").setValue("+7111111111");
         $("[data-test-id=agreement]").click();
         $("button.button.button_view_extra.button_size_m.button_theme_alfa-on-white").click();
         $("[data-test-id=order-success]").shouldHave(text("Ваша заявка успешно отправлена!"));
